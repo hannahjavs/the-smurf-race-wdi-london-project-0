c46413b071b4d42d1e160b17493ba47cd5cf36e0
hannahjavs <hannahsiujadavji@gmail.com>
2017-08-26 11:57:14 +0100

    Update 26/08/17

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,36 @@
-console.log('oi');
+$(() => {
+
+  const $gamePane = $('#gamePane');
+  const $smurf1 = $('#smurf1');
+  const $smurf2 = $('#smurf2');
+
+
+  // FUNCTIONS HERE
+
+
+  $(window).keyup(function(e) {
+    if(e.which === 188) { // if , left is pressed and then . right
+      console.log('right');
+      const currentPosition = $smurf1.css('left');
+      console.log(currentPosition);
+      $smurf1.animate({ left: '+=10' }, 'fast');
+      if(currentPosition === '900px') alert('Smurf wins!!!');
+    }
+  });
+
+  $(window).keyup(function(e) {
+    if(e.which === 88) { // if , left is pressed and then . right
+      console.log('right');
+      const currentPosition = $smurf2.css('left');
+      console.log(currentPosition);
+      $smurf2.animate({ left: '+=10' }, 'fast');
+      if(currentPosition === '900px') alert('Smurfette wins!!!');
+    }
+  });
+
+
+
+// BUTTONS HERE
+
+
+});
